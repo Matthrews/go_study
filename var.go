5dec1bfa09151f10efbf74e21f510a0172066ad1
Oliver <matthrews@outlook.com>
2022-02-28 23:51:54 +0800

    4. 变量声明

---
+++ var.go
@@ -0,0 +1,15 @@
+package main
+
+//类型相同多个变量, 非全局变量
+var vname1, vname2, vname3 = v1, v2, v3
+
+var vname1, vname2, vname3 = v1, v2, v3 // 和 python 很像,不需要显示声明类型，自动推断
+
+vname1, vname2, vname3 := v1, v2, v3 // 出现在 := 左侧的变量不应该是已经被声明过的，否则会导致编译错误
+// 只能被用在函数体内，而不可以用于全局变量的声明与赋值
+
+// 这种因式分解关键字的写法一般用于声明全局变量
+var (
+    vname1 v_type1
+    vname2 v_type2
+)
