970f39f61adeb783655011e5b89b2882a8e769c0
Oliver <zhurongfu@opsmind.com>
2022-04-13 18:29:17 +0800

    17. parse float

--- day07/user_input/main.go
+++ day07/user_input/main.go
@@ -4,11 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func getInput(prompt string, r *bufio.Reader) (string, error) {
-	fmt.Println(prompt)
+	fmt.Print(prompt)
 	input, err := r.ReadString('\n')
 
 	return strings.TrimSpace(input), err
@@ -16,24 +17,54 @@ func getInput(prompt string, r *bufio.Reader) (string, error) {
 
 func createbill() bill {
 	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Print("Create a new bill name: ")
-	name, _ := reader.ReadString('\n')
-	name = strings.TrimSpace(name)
+	name, _ := getInput("Create a new bill name: ", reader)
 
 	return newBill(name)
 }
 
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+	opt, _ := getInput("Choose options(a - add item, s - save bill, t - add tip): ", reader)
+
+	// fmt.Println("getInput", opt)
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		p, err := strconv.ParseFloat(price, 64)
+
+		if err != nil {
+			fmt.Println("the price must be a number")
+			promptOptions(b)
+		}
+		b.addItem(name, p)
+		fmt.Println("Item added - ", name, p)
+		promptOptions(b)
+	case "t":
+		tip, _ := getInput("Enter tip amount ($): ", reader)
+		t, err := strconv.ParseFloat(tip, 64)
+
+		if err != nil {
+			fmt.Println("the tip must be a number")
+			promptOptions(b)
+		}
+		b.updateTip(t)
+		fmt.Println("tip added - ", t)
+		promptOptions(b)
+	case "s":
+		fmt.Println("you chose to save the bill", b)
+
+	default:
+		fmt.Println("that was not a valid option...")
+		promptOptions(b)
+	}
+
+}
+
 func main() {
 	myBill := createbill()
-
-	// myBill.updateTip(10.00)
-
-	// myBill.addItem("peer", 5.68)
-
-	// formattedBill := myBill.format()
-
-	fmt.Println(myBill)
+	promptOptions(myBill)
 }
 
 // go run day07/user_input/main.go day07/user_input/bill.go
